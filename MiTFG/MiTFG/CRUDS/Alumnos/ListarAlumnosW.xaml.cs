6e8d2883ebf81058d60d1d18f8a4e62709ff1205
Francisco Javier Andreu García <javiandreugarcia@gmail.com>
2024-05-29 14:01:41 +0200

    update

--- MiTFG/MiTFG/CRUDS/Alumnos/ListarAlumnosW.xaml.cs
+++ MiTFG/MiTFG/CRUDS/Alumnos/ListarAlumnosW.xaml.cs
@@ -29,43 +29,12 @@ namespace MiTFG
 
         private void CargarAlumnos()
         {
-            DAOAlumno daoAlumno = new DAOAlumno();
-            DAOTutores daoTutores = new DAOTutores();
-            List<Alumno> alumnos = daoAlumno.ObtenerAlumnos();
-            List<Tutor> tutores = daoTutores.ObtenerTutores();
-
-            // Mapear el nombre del tutor a cada alumno
-            var alumnosConTutores = from alumno in alumnos
-                                    join tutor in tutores on alumno.TutoresID equals tutor.ID
-                                    select new
-                                    {
-                                        alumno.ID,
-                                        alumno.Nombre,
-                                        alumno.Apellidos,
-                                        alumno.DNI,
-                                        alumno.Email,
-                                        alumno.NumeroTelefono,
-                                        alumno.Curso,
-                                        alumno.FechaDeNacimiento,
-                                        TutorNombre = tutor.Nombre
-                                    };
-
-            dgAlumnos.ItemsSource = alumnosConTutores.ToList();
-        }
-
-        private void btnEditar_Click(object sender, RoutedEventArgs e)
-        {
-
+            
         }
 
         private void btnEliminar_Click(object sender, RoutedEventArgs e)
         {
 
-        }
-
-        private void btnActualizar_Click(object sender, RoutedEventArgs e)
-        {
-            CargarAlumnos();
         }
     }
 }
