f144dd9f015a286c18a687c5009c4cd0fa1d75fd
Francisco Javier Andreu García <javiandreugarcia@gmail.com>
2024-06-03 10:15:54 +0200

    delete Usuarios
    He añadido al grid de usuarios la opcion de eliminarlo desde ahi como en las demas vistas de listado

--- MiTFG/MiTFG/CRUDS/Usuarios/ListarUsuarios.xaml.cs
+++ MiTFG/MiTFG/CRUDS/Usuarios/ListarUsuarios.xaml.cs
@@ -28,6 +28,19 @@ namespace MiTFG.CRUDS.Usuarios
             DAOUsuario daoUsuario = new DAOUsuario();
             List<Usuario> usuarios = daoUsuario.obtenerUsuarios();
             dgUsuarios.ItemsSource = usuarios;
-        }        
+        }
+
+        private void btnEliminarUsuario_Click(object sender, RoutedEventArgs e)
+        {
+            int alumnoID = (int)((Button)sender).Tag;  //Tag almacena el ID del alumno de la linea en la que se ha seleccionado el boton eliminar
+
+            MessageBoxResult result = MessageBox.Show("¿Estás seguro de que deseas eliminar este tutor?", "Confirmar eliminación", MessageBoxButton.YesNo, MessageBoxImage.Warning);
+            if (result == MessageBoxResult.Yes)
+            {
+                DAOUsuario daoUsuario = new DAOUsuario();
+                daoUsuario.eliminarUsuario(alumnoID);
+                CargarUsuarios();
+            }
+        }
     }
 }
