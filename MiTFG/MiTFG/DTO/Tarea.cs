b1f2ff2cff01d69391596e25f08d39a74a70c671
Francisco Javier Andreu García <javiandreugarcia@gmail.com>
2024-06-04 09:44:46 +0200

    update class tarea
    Tenia campos innecesarios de una version antigua de la bdd

--- MiTFG/MiTFG/DTO/Tarea.cs
+++ MiTFG/MiTFG/DTO/Tarea.cs
@@ -9,22 +9,20 @@ namespace MiTFG.DTO
     internal class Tarea
     {
         public int ID { get; set; }
-        public string Nombre {  get; set; }
+        public string Nombre { get; set; }
         public string Tipo { get; set; }
-        public int IdAlumno { get; set; }
-        public decimal Nota { get; set; }
         public string Comentario { get; set; }
+        public int Cursos_ID { get; set; }
 
         public Tarea() { }
 
-        public Tarea(int id, string nombre, string tipo, int idAlumno, decimal nota, string comentario)
+        public Tarea(int id, string nombre, string tipo, string comentario, int cursos_ID)
         {
             ID = id;
             Nombre = nombre;
             Tipo = tipo;
-            IdAlumno = idAlumno;
-            Nota = nota;
             Comentario = comentario;
+            Cursos_ID = cursos_ID;
         }
     }
 }
