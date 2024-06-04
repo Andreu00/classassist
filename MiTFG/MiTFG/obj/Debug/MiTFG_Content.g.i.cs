7b47a5b8b4c7db71b711ed39d68f02e330ed3b37
Francisco Javier Andreu García <javiandreugarcia@gmail.com>
2024-06-04 13:50:16 +0200

    change icon
    He añadido el logotipo al icono de la app

--- MiTFG/MiTFG/obj/Debug/MiTFG_Content.g.i.cs
+++ MiTFG/MiTFG/obj/Debug/MiTFG_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("logotipo.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("iconoapp.ico")]
 
 
